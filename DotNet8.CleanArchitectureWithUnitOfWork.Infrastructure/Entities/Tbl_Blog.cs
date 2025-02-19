0cc47d65eb9c212861a42a7ef492433390b7337e
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-19 18:58:11 +0630

    Feat : Add Tbl_Blog

--- DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure/Entities/Tbl_Blog.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure/Entities/Tbl_Blog.cs
@@ -2,4 +2,8 @@
 
 public class Tbl_Blog
 {
+	public int BlogId { get; set; }
+	public string BlogTitle { get; set; }
+	public string BlogAuthor { get; set; }
+	public string BlogContent { get; set; }
 }
