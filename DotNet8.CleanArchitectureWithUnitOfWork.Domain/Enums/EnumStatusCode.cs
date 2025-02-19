7399d2057061d4e685afdd3cda5833210e25c7e3
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-19 13:27:19 +0630

    Feat : Add EnumStatusCode

--- DotNet8.CleanArchitectureWithUnitOfWork.Domain/Enums/EnumStatusCode.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Domain/Enums/EnumStatusCode.cs
@@ -1,5 +1,12 @@
 ﻿namespace DotNet8.CleanArchitectureWithUnitOfWork.Domain.Enums;
 
-public class EnumStatusCode
+public enum EnumStatusCode
 {
+	Success = 200,
+	Created = 201,
+	Accepted = 202,
+	BadRequest = 400,
+	NotFound = 404,
+	Duplicate = 409,
+	InternalServerError = 500
 }
