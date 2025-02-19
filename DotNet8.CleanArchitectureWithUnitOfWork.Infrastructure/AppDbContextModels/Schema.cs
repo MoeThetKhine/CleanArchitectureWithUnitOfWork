3d9979df532ad3c3b7537a66920a155c175a7420
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-19 20:43:28 +0630

    -

---
+++ DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure/AppDbContextModels/Schema.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure.AppDbContextModels;
+
+public partial class Schema
+{
+    public int Version { get; set; }
+}
