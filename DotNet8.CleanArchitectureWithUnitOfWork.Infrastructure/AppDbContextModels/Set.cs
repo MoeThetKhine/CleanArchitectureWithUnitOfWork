e60909cc17334884d4dfdfdc90e0a7a429f86566
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-19 20:43:51 +0630

    -

---
+++ DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure/AppDbContextModels/Set.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure.AppDbContextModels;
+
+public partial class Set
+{
+    public string Key { get; set; } = null!;
+
+    public double Score { get; set; }
+
+    public string Value { get; set; } = null!;
+
+    public DateTime? ExpireAt { get; set; }
+}
