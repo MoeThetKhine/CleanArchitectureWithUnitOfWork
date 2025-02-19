5044ebf3ccab28f3c5847c9c41ed1014ce7d69c0
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-19 11:11:04 +0630

    -

--- DotNet8.CleanArchitectureWithUnitOfWork.Domain/Enums/EnumStatusCode.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Domain/Enums/EnumStatusCode.cs
@@ -1,6 +1,4 @@
-﻿using System.Threading.Tasks;
-
-namespace DotNet8.CleanArchitectureWithUnitOfWork.Domain.Enums
+﻿namespace DotNet8.CleanArchitectureWithUnitOfWork.Domain.Enums
 {
 	internal class EnumStatusCode
 	{
