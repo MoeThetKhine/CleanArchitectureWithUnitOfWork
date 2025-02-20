d1db58cd154d2ddd34b49914e8aa8177ca74fd81
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-20 12:13:28 +0630

    Format Code

--- DotNet8.CleanArchitectureWithUnitOfWork.Api/Features/Blog/BlogController.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Api/Features/Blog/BlogController.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet8.CleanArchitectureWithUnitOfWork.Api.Features.Blog
+namespace DotNet8.CleanArchitectureWithUnitOfWork.Api.Features.Blog;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : ControllerBase
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class BlogController : ControllerBase
-	{
-	}
 }
