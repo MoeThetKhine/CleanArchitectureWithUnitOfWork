5097895c9f8bcabd9e4ad91698f0aad19f67eec6
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-20 20:41:13 +0630

    -

--- DotNet8.CleanArchitectureWithUnitOfWork.Api/ChangeModel.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Api/ChangeModel.cs
@@ -1,5 +1,18 @@
-﻿namespace DotNet8.CleanArchitectureWithUnitOfWork.Api;
+﻿using DotNet8.CleanArchitectureWithUnitOfWork.Domain.Models.Blog;
+using DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure.AppDbContextModels;
 
-public class ChangeModel
+namespace DotNet8.CleanArchitectureWithUnitOfWork.Api;
+
+public static class ChangeModel
 {
+	public static BlogModel Change(this TblBlog dataModel)
+	{
+		return new BlogModel
+		{
+			BlogId = dataModel.BlogId,
+			BlogTitle = dataModel.BlogTitle,
+			BlogAuthor = dataModel.BlogAuthor,
+			BlogContent = dataModel.BlogContent,
+		};
+	}
 }
