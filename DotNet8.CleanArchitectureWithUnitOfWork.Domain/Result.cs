3a5cd5b66cad2cb386e492d721708148ca806cb3
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-20 11:19:25 +0630

    Feat : Add Result

--- DotNet8.CleanArchitectureWithUnitOfWork.Domain/Result.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Domain/Result.cs
@@ -1,5 +1,12 @@
-﻿namespace DotNet8.CleanArchitectureWithUnitOfWork.Domain;
+﻿using DotNet8.CleanArchitectureWithUnitOfWork.Domain.Enums;
 
-public class Result
+namespace DotNet8.CleanArchitectureWithUnitOfWork.Domain;
+
+public class Result<T>
 {
+	public T Data { get; set; }
+	public bool IsSuccess { get; set; }
+	public bool IsError { get { return !IsSuccess; } }
+	public string Message { get; set; }
+	public EnumStatusCode StatusCode { get; set; }
 }
