6bba6e2eaa5289c600f5abbe02322337d83bc9fc
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-20 11:15:48 +0630

    -

--- DotNet8.CleanArchitectureWithUnitOfWork.Domain/Models/Blog/BlogListResponseModel.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Domain/Models/Blog/BlogListResponseModel.cs
@@ -1,6 +1,10 @@
 ﻿namespace DotNet8.CleanArchitectureWithUnitOfWork.Domain.Models.Blog;
 
+#region BlogListResponseModel
+
 public class BlogListResponseModel
 {
 	public List<BlogModel> Blogs { get; set; }
 }
+
+#endregion
