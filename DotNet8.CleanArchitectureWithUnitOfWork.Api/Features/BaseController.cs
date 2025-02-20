a899583ed6d764f07f837d6883bf12c13e8770d7
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-20 21:09:42 +0630

    Feat : Add Content

--- DotNet8.CleanArchitectureWithUnitOfWork.Api/Features/BaseController.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Api/Features/BaseController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace DotNet8.CleanArchitectureWithUnitOfWork.Api.Features;
 
@@ -6,4 +7,8 @@ namespace DotNet8.CleanArchitectureWithUnitOfWork.Api.Features;
 [ApiController]
 public class BaseController : ControllerBase
 {
+	protected IActionResult Content(object obj)
+	{
+		return Ok(JsonConvert.SerializeObject(obj));
+	}
 }
