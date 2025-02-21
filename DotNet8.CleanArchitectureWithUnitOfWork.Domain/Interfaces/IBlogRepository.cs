62da6c46134abd72bdb388daf2806c37cf8b8776
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-21 13:18:08 +0630

    Feat : Add GetBlogListAsync in IBlogRepository

--- DotNet8.CleanArchitectureWithUnitOfWork.Domain/Interfaces/IBlogRepository.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Domain/Interfaces/IBlogRepository.cs
@@ -1,5 +1,8 @@
-﻿namespace DotNet8.CleanArchitectureWithUnitOfWork.Domain.Interfaces;
+﻿using DotNet8.CleanArchitectureWithUnitOfWork.Domain.Models.Blog;
+
+namespace DotNet8.CleanArchitectureWithUnitOfWork.Domain.Interfaces;
 
 public interface IBlogRepository
 {
+	Task<Result<BlogListResponseModel>> GetBlogListAsync();
 }
