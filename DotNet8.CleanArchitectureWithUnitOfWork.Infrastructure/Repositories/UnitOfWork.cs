be22df6d9d9d254ae353696a04075c527e7ee131
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-21 13:18:52 +0630

    Feat : Add UnitOfWork

--- DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure/Repositories/UnitOfWork.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure/Repositories/UnitOfWork.cs
@@ -1,5 +1,18 @@
-﻿namespace DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure.Repositories;
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure.Repositories;
 
 public class UnitOfWork : IUnitOfWork
 {
+	private IBlogRepository _blogRepository;
+	private IServiceProvider _serviceProvider;
+
+	public UnitOfWork(IBlogRepository blogRepository, IServiceProvider serviceProvider)
+	{
+		_blogRepository = blogRepository;
+		_serviceProvider = serviceProvider;
+	}
+
+	public IBlogRepository BlogRepository =>
+	   _blogRepository ??= _serviceProvider.GetRequiredService<IBlogRepository>();
 }
