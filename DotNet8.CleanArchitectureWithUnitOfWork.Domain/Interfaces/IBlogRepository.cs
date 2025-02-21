abf410acdb3a3bb0577c764cc6af5b1868cefe25
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-21 13:27:52 +0630

    -

--- DotNet8.CleanArchitectureWithUnitOfWork.Domain/Interfaces/IBlogRepository.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Domain/Interfaces/IBlogRepository.cs
@@ -1,6 +1,10 @@
 ﻿namespace DotNet8.CleanArchitectureWithUnitOfWork.Domain.Interfaces;
 
+#region IBlogRepository
+
 public interface IBlogRepository
 {
 	Task<Result<BlogListResponseModel>> GetBlogListAsync();
 }
+
+#endregion
