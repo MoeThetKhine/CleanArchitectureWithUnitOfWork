7c011845121ea4dbd96b39f75fc1f1fda9898905
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-21 13:25:06 +0630

    Feat : Add Service in Program

--- DotNet8.CleanArchitectureWithUnitOfWork.Api/Program.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Api/Program.cs
@@ -1,9 +1,13 @@
+using DotNet8.CleanArchitectureWithUnitOfWork.Api;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddFeatures(builder);
 
 var app = builder.Build();
 
