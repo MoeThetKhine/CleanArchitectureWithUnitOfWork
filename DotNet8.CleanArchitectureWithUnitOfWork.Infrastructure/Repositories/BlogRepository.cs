471a46f5fccf6683f239cbb28aedc7d137a41533
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-21 13:19:20 +0630

    Feat : Add GetBlogListAsync in BlogRepository

--- DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure/Repositories/BlogRepository.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure/Repositories/BlogRepository.cs
@@ -1,5 +1,36 @@
-﻿namespace DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure.Repositories;
+﻿using DotNet8.CleanArchitectureWithUnitOfWork.Domain;
+using DotNet8.CleanArchitectureWithUnitOfWork.Domain.Models.Blog;
+using DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure.AppDbContextModels;
+
+namespace DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure.Repositories;
 
 public class BlogRepository : IBlogRepository
 {
+	private readonly AppDbContext _context;
+
+	public BlogRepository(AppDbContext context)
+	{
+		_context = context;
+	}
+
+	public async Task<Result<BlogListResponseModel>> GetBlogListAsync()
+	{
+		Result<BlogListResponseModel> result;
+
+		try
+		{
+			var lst = await _context.TblBlogs
+				.OrderByDescending(x => x.BlogId)
+				.ToListAsync();
+
+			var model = new BlogListResponseModel { Blogs = lst.Select(x => x.Change()).ToList() };
+
+			result =  Result<BlogListResponseModel>.SuccessResult(model);
+		}
+		catch (Exception ex)
+		{
+			result = Result<BlogListResponseModel>.FailureResult(ex);
+		}
+		return result;
+	}
 }
