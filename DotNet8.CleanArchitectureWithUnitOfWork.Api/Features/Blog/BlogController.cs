1b42aa4c5537566ad71187c01eebbc67ce60efa5
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-21 13:23:52 +0630

    Feat : Add GetBlogListAsync in Controller

--- DotNet8.CleanArchitectureWithUnitOfWork.Api/Features/Blog/BlogController.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Api/Features/Blog/BlogController.cs
@@ -4,6 +4,26 @@ namespace DotNet8.CleanArchitectureWithUnitOfWork.Api.Features.Blog;
 
 [Route("api/[controller]")]
 [ApiController]
-public class BlogController : ControllerBase
+public class BlogController : BaseController
 {
+	private IUnitOfWork _unitOfWork;
+
+	public BlogController(IUnitOfWork unitOfWork)
+	{
+		_unitOfWork = unitOfWork;
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetBlogListAsync()
+	{
+		try
+		{
+			var result = await _unitOfWork.BlogRepository.GetBlogListAsync();
+			return Content(result);
+		}
+		catch (Exception ex)
+		{
+			return InternalServerError(ex);
+		}
+	}
 }
