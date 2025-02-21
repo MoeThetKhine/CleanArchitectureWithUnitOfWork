486bd9ed349ca56c01051f2d40241efdbebcfec8
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-21 13:29:42 +0630

    -

--- DotNet8.CleanArchitectureWithUnitOfWork.Domain/Interfaces/IUnitOfWork.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Domain/Interfaces/IUnitOfWork.cs
@@ -1,6 +1,10 @@
 ﻿namespace DotNet8.CleanArchitectureWithUnitOfWork.Domain.Interfaces;
 
+#region IUnitOfWork
+
 public interface IUnitOfWork
 {
 	IBlogRepository BlogRepository { get; }
 }
+
+#endregion
