5233697d825daa27b3939972d9a824e1962b2afc
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-22 15:35:17 +0630

    Feat : Add BlogResponseModel

--- DotNet8.CleanArchitectureWithUnitOfWork.Domain/Models/Blog/BlogResponseModel.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Domain/Models/Blog/BlogResponseModel.cs
@@ -2,4 +2,9 @@
 
 public class BlogResponseModel
 {
+	public string BlogTitle { get; set; } = null!;
+
+	public string BlogAuthor { get; set; } = null!;
+
+	public string BlogContent { get; set; } = null!;
 }
